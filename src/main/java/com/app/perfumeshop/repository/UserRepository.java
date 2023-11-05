21c3d3bde72c0a57dfd1f6ee181ea6df0d2d2fb8
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-11-05 12:42:49 +0200

    Added User register validations

--- src/main/java/com/app/perfumeshop/repository/UserRepository.java
+++ src/main/java/com/app/perfumeshop/repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.app.perfumeshop.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String username);
+
+    Optional<User> findUserByUsername(String username);
 }
