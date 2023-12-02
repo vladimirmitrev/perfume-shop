3844e7f52ef93bce7c6c125107e552a4ed76d31c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-12-02 20:11:54 +0200

    Added testRegistrationUnsuccessful integration test

--- src/test/java/com/app/perfumeshop/web/UserControllerTestIT.java
+++ src/test/java/com/app/perfumeshop/web/UserControllerTestIT.java
@@ -1,16 +1,19 @@
 package com.app.perfumeshop.web;
 
+import com.app.perfumeshop.repository.UserRepository;
+import com.app.perfumeshop.util.TestDataUtils;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 @SpringBootTest
@@ -20,8 +23,17 @@ class UserControllerTestIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private TestDataUtils testDataUtils;
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    void tearDown() {
+//        userRepository.deleteAll();
+    }
     @Test
-    void testRegistration() throws Exception {
+    void testRegistrationSuccessful() throws Exception {
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/users/register")
                         .param("email", "pesho@softuni.bg")
@@ -34,5 +46,20 @@ class UserControllerTestIT {
                         .with(csrf())
         ).andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/users/login"));
+    }
+    @Test
+    void testRegistrationUnsuccessful() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users/register")
+                                .param("email", "pesho@softuni.bg")
+                                .param("username", "pesho")
+                                .param("firstName", "Pesho")
+                                .param("lastName", "Petrov")
+                                .param("phoneNumber", "0888123456")
+                                .param("password", "password")
+                                .param("confirmPassword", "password")
+                                .with(csrf())
+                ).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/register"));
     }
 }
